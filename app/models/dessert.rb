253b1dddc30e6f61eaf438e15514252ba2edbf75
github username <crishanks12@gmail.com>
2019-02-03 17:41:49 -0800

    ruby classes

--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -1,9 +1,23 @@
 class Dessert
     @@all = []
-    attr_accessor :name
+    attr_accessor :name, :bakery
 
-    def initialize(name)
+    def initialize(name, bakery)
         @name = name
+        @bakery = bakery
+        @@all << self
+    end
+
+    def ingredients
+        Ingredient.all.select {|ingredient| ingredient.dessert == self}
+    end
+
+    def calories
+        total = 0
+        self.ingredients.each do |ingredient|
+            total += ingredient.calories
+        end
+        total
     end
 
     def self.all
--- app/models/ingredients.rb
+++ app/models/ingredients.rb
@@ -1,13 +1,20 @@
+require 'pry'
+
 class Ingredient
     @@all = []
     attr_accessor :calories, :name, :bakery, :dessert
 
-    def initialize(calories, name, bakery, dessert)
+    def initialize(name, calories, bakery, dessert)
         @calories = calories
         @name = name
         @bakery = bakery
         @dessert = dessert
-        @@all << all
+        @@all << self
+    end
+
+    def self.find_all_by_name(ingredient_name)
+        Ingredient.all.select {|ingredient| ingredient.name.include?(ingredient_name)}
+
     end
 
     def self.all
