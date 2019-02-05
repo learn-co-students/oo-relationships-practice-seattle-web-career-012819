66050d13d4b7a2d5ab15b007c9ccce5ce65c7bc1
github username <github email address>
2019-02-05 10:07:17 -0800

    Done.

---
+++ app/models/ride.rb
@@ -0,0 +1,30 @@
+class Ride
+  attr_accessor :driver, :passenger, :distance
+  @@all =[]
+  #@@distance = []
+  def initialize(driver, passenger, distance)
+    #@@distance << distance
+    @driver = driver
+    @passenger = passenger
+    @distance =  distance
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+   def self.average_distance
+     average = 0
+     count = 0
+     @@all.each do |find_distance|
+       average += find_distance.distance
+       count += 1
+     end
+  #   average = 0
+  #   @@distance.each do |miles|
+  #     average += miles
+  #   end
+     average / count
+   end
+end
