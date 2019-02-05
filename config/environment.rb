70749cf49b76cff0888c23fbbed0e288b3322fa1
Tiffany <Tafby88@gmail.com>
2019-02-05 10:16:30 -0800

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,11 @@ Bundler.require
 require_relative '../app/models/guests.rb'
 require_relative '../app/models/listings.rb'
 require_relative '../app/models/trips.rb'
+
+require_relative '../app/models/ride.rb'
+require_relative '../app/models/driver.rb'
+require_relative '../app/models/passenger.rb'
+
+require_relative '../app/models/bakery.rb'
+require_relative '../app/models/desserts.rb'
+require_relative '../app/models/ingredients.rb'
--- tools/console.rb
+++ tools/console.rb
@@ -17,20 +17,37 @@ end
 # d4 = Desserts.new("Waffle", b1,  [i4,i1] )
 
 
-g1 = Guests.new("Frank")
-g2 = Guests.new("Susan")
-g3 = Guests.new("Eileen")
-g4 = Guests.new("Jessica")
+# g1 = Guests.new("Frank")
+# g2 = Guests.new("Susan")
+# g3 = Guests.new("Eileen")
+# g4 = Guests.new("Jessica")
 
-l1 = Listings.new("Seattle")
-l2 = Listings.new("Atlanta")
-l3 = Listings.new("Miami")
-l4 = Listings.new("Seattle")
+# l1 = Listings.new("Seattle")
+# l2 = Listings.new("Atlanta")
+# l3 = Listings.new("Miami")
+# l4 = Listings.new("Seattle")
 
-t1 = Trips.new(l1, g1)
-t2 = Trips.new(l2, g1)
-t3 = Trips.new(l3, g3)
-t4 = Trips.new(l4, g4)
+# t1 = Trips.new(l1, g1)
+# t2 = Trips.new(l2, g1)
+# t3 = Trips.new(l3, g3)
+# t4 = Trips.new(l4, g4)
+
+p1 = Passenger.new("Bob")
+p2 = Passenger.new("Sally")
+p3 = Passenger.new("Sue")
+p4 = Passenger.new("Tilly")
+
+d1 = Driver.new("Alex")
+d2 = Driver.new("Josh")
+d3 = Driver.new("Molly")
+d4 = Driver.new("Greyson")
+
+r1 = Ride.new(p1, d1, 2)
+r2 = Ride.new(p1, d2, 3)
+r3 = Ride.new(p2, d2, 3)
+r4 = Ride.new(p3, d3, 4)
+r1 = Ride.new(p4, d4, 105)
+
 
 
 binding.pry
